3051ec2f28629d351346a30ed178ee872dd50810
Hyung <h9564289@gmail.com>
2025-03-10 23:57:21 +0900

    헤더 설정

--- api/index.js
+++ api/index.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors({
   origin: 'https://restaurant-search-mu.vercel.app',
   methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use(express.json());
