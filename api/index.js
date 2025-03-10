8526da72a1e14a44c738f896d8e854fd9aec3e07
Hyung <h9564289@gmail.com>
2025-03-11 00:22:22 +0900

    카카오 REDIRECT_URI 오타 수정

--- api/index.js
+++ api/index.js
@@ -61,7 +61,7 @@ app.get('/api/search', (req, res) => {
 
 app.get("/auth/Kakao", async (req, res) => {
   let REST_API_KEY = process.env.REACT_APP_KAKAO_REST_API_KEY;
-  let REDIRECT_URI = 'https://restaurant-search-mu.vercel.app/gLogin';
+  let REDIRECT_URI = 'https://restaurant-search-mu.vercel.app/Login';
 
   let code = req.query.code;
   console.log("인가 코드:", code);
