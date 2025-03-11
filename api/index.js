fba882f7424d550afe177f99cc343bd66bc8134c
Hyung <h9564289@gmail.com>
2025-03-11 20:22:31 +0900

    카카오 경로 수정

--- api/index.js
+++ api/index.js
@@ -59,7 +59,7 @@ app.get('/api/search', (req, res) => {
   fetchData();
 });
 
-app.get("/auth/Kakao", async (req, res) => {
+app.get("/api/auth/Kakao", async (req, res) => {
   let REST_API_KEY = process.env.REACT_APP_KAKAO_REST_API_KEY;
   let REDIRECT_URI = 'https://restaurant-search-mu.vercel.app/Login';
 
